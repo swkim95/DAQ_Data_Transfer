a360cfd1ad33629d7c6b5927b27fd34bb774a5a7
swkim95 <sungwon.kim@cern.ch>
2022-11-09 05:40:16 +0900

    For python3 env

--- Remove_Data.sh
+++ Remove_Data.sh
@@ -8,4 +8,4 @@ SRC_BASENAME=$(echo $SRC_DIR | awk -F/ '{print $NF}')
 LOG_FILE="./Log/Remove_Log/Log_$SRC_BASENAME.txt"
 
 echo "\033[94m[INFO]\033[0m Log file will be created with path :\033[96m\033[1m $LOG_FILE \033[0m"
-python remove_data.py ${SRC_DIR} ${DEST_DIR} | tee -a ${LOG_FILE} 
+python3 remove_data.py ${SRC_DIR} ${DEST_DIR} | tee -a ${LOG_FILE} 
